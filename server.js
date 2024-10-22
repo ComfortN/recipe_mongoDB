87bdb0430b0a72c07828c784dee8d77c133adc95
Comfort Ngwenya <>
2024-10-22 20:22:43 +0200

    Adding the use off router on the server

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import "dotenv/config"
 import express from "express"
 import cors from "cors"
 import connectDB from "./config/database.js"
+import router from "./routes/api.js"
 
 const app = express();
 const PORT = process.env.PORT || 8080
@@ -9,6 +10,7 @@ const PORT = process.env.PORT || 8080
 connectDB()
 app.use(cors())
 app.use(express.json());
+app.use('/api', router)
 
 
 
