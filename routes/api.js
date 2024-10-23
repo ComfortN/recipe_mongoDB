f1da807fa243a7ca008dca6ade2550c4d277f1ef
Comfort Ngwenya <>
2024-10-23 14:46:40 +0200

    Adding the validations to the routes

--- routes/api.js
+++ routes/api.js
@@ -1,13 +1,14 @@
 import express from "express"
 import recipeController from "../controllers/recipeController.js"
+import { validateCreateRecipe, validatePagination, validateRecipeId, validateUpdateRecipe } from "../middleware/recipeValidation.js"
 
 const router = express.Router()
 
-router.post('/recipes', recipeController.CreateRecipe)
-router.get('/recipes', recipeController.getRecipes)
-router.get('/recipes/:id', recipeController.getRecipe)
-router.put('/recipes/:id', recipeController.updateRecipe)
-router.delete('/recipes/:id', recipeController.deleteRecipe)
+router.post('/recipes', validateCreateRecipe, recipeController.CreateRecipe)
+router.get('/recipes', validatePagination, recipeController.getRecipes)
+router.get('/recipes/:id', validateRecipeId, recipeController.getRecipe)
+router.put('/recipes/:id', [...validateRecipeId, validateUpdateRecipe], recipeController.updateRecipe)
+router.delete('/recipes/:id', validateRecipeId, recipeController.deleteRecipe)
 
 
 export default router;
